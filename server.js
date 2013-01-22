31db46794fe001498e7add4fb938aeb5886fed82
Danchenkov Egor <icepnyx@gmail.com>
2013-01-22 23:16:11 +0200

    Improvements

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
+//connect http-module
 var http = require("http");
 
-http.createServer(function(request, response) {
-  response.writeHead(600, {"Content-Type": "text/plain"});
-  response.write("Hello World  ! Node.js");
-  response.end();
-}).listen(8888);
+var url = require("url");
+
+//request and responds are obejcts
+//we can methods to work with them
+function start(route) {
+	function onRequest(request, response) {
+	  
+	  //now we can define different requst based on 
+	  //different URLs
+	  var pathname = url.parse(request.url).pathname;
+      console.log("Request for " + pathname + " received.");
+      
+      route(pathname);
+      
+      response.writeHead(200, {"Content-Type": "text/plain"})
+	  response.write("Hello World");
+	  response.end();
+	}
+
+	http.createServer(onRequest).listen(8888);
+
+	console.log("Server has started.");
+}
+
+
+exports.start = start;
