e3cf39c725c18d73aca47d1348c347e3cbda01b5
olshakurova <olshakurova@olshakurova.(none)>
2013-03-29 16:52:20 +0200

    simple pagination

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,23 @@
 var models = require('../dbModels')
 
-exports.index = function( req, res ){ 
+exports.index = function( req, res, next){ 
 	//Можно это проверять тут и передавать как boll авторизацию
-	 	  models.Bug.find({}).populate('author', 'username').exec(function (err, bugs) {
-      req.bugs = bugs;
-      res.render('index', {req: req});
-       });
-     
+	 var pageList = 3;
+     var page = req.query.page && parseInt(req.query.page, 10) || 0;
+    models.Bug.count(function (err, count){
+    	if(err){
+    		return next(err);
+    	}
+         var lastPage = (page+1)*pageList >=count;
+         models.Bug.find({})
+	 	    .populate('author', 'username')
+	 	    .skip(pageList * page)
+            .limit(pageList)
+            .sort({createdAt: 1})
+	 	    .exec(function (err, bugs) {
+	 		    req.bugs = bugs;
+                res.render('index', {req: req, page: page, lastPage: lastPage});
+            });
+	 	})
 }
  
