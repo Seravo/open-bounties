2e4d30aef5c4638c0706aaac92c1ce5f1099c6ec
olshakurova <olshakurova@olshakurova.(none)>
2013-03-30 10:01:39 +0200

    with some changes in pagination

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,15 @@ var models = require('../dbModels')
 exports.index = function( req, res, next){ 
 	//Можно это проверять тут и передавать как bool авторизацию
 	 var pageList = 3;
-     var page = req.query.page && parseInt(req.query.page, 3) || 0;
+     var page = req.query.page && parseInt(req.query.page, 10) || 0;
     models.Bug.count(function (err, count){
     	if(err){
     		return next(err);
     	}
-         var lastPage = (page+1)*pageList >=count;
+         var lastPage = Math.ceil(count/pageList)-1
+         console.log(page)
+         console.log(lastPage)
+         console.log(count)
          models.Bug.find({})
 	 	    .populate('author', 'username')
 	 	    .skip(pageList * page)
